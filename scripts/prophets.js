cd6e82461ddc428f918f529a92a58a5513cc791e
matip31 <matiasmiguel31@gmail.com>
2026-07-09 10:27:46 -0300

    learning

---
+++ scripts/prophets.js
@@ -0,0 +1,31 @@
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+async function getProphetData(){
+    const response= await fetch(url);
+    const data= await response.json();
+    // console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+getProphetData()
+function displayProphets(prophets){
+    prophets.forEach((prophet)=>{
+        const card=document.createElement("section");
+        const fullName=document.createElement("h2");
+        const portrait=document.createElement("img");
+        const birthDate=document.createElement("p");
+        const birthPlace=document.createElement("p");
+        birthDate.textContent=`Date of Birth: ${prophet.birthdate}`;
+        birthPlace.textContent=`Place of Birth: ${prophet.birthplace}`;
+        fullName.textContent=`${prophet.name} ${prophet.lastname}`;
+        portrait.src=prophet.imageurl;
+        portrait.alt="Prophet Picture";
+        portrait.loading="lazy";
+        portrait.width=200;
+        portrait.height=250;
+        card.appendChild(fullName);
+        card.appendChild(birthDate);
+        card.appendChild(birthPlace);
+        card.appendChild(portrait);
+        cards.appendChild(card);
+    });
+}
